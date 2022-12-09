e5a27fd5eec99c0bcb9573e085e1f5d4f3fa2c30
cpi2544 <pokpong1789@gmail.com>
2022-12-09 15:12:14 +0700

    completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 int main(){
+
+    int i;
+    int E=0;
+    int O=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> i;
+
+    while (i!=0)
+    {
+        cout << "Enter an integer: ";
+        cin >> i;
+        if(i%2==0){
+            O++;
+        }else{
+            E++;
+        }
+    }
+
+    
+
+    cout << "#Even numbers = " << E << "\n";
+    cout << "#Odd numbers = " << O;
     return 0;
 }
